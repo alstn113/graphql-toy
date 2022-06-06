ffc008e269537d2930b20552b2f81118c93dc21e
neko113 <alstn113@gmail.com>
2022-06-07 00:15:32 +0900

    feat : add database(mysql)

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -4,9 +4,20 @@ import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { AppResolver } from './app.resolver';
 import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
 import { CatsModule } from './cats/cats.module';
+import { ConfigModule } from '@nestjs/config';
+import configuration from './config/configuration';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DatabaseConfigService } from './config/database.config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configuration],
+    }),
+    TypeOrmModule.forRootAsync({
+      useClass: DatabaseConfigService,
+    }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       typePaths: ['./**/*.graphql'],
