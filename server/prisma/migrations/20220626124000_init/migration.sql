f7868dddb812c596bdf8b01f80fa0266c9aee5f9
neko113 <alstn113@gmail.com>
2022-06-26 21:40:42 +0900

    feat : add user schema

--- server/prisma/migrations/20220626124000_init/migration.sql
+++ server/prisma/migrations/20220626124000_init/migration.sql
@@ -1,4 +1,16 @@
 -- CreateTable
+CREATE TABLE `User` (
+    `id` VARCHAR(191) NOT NULL,
+    `socialId` VARCHAR(191) NOT NULL,
+    `username` VARCHAR(191) NOT NULL,
+    `provider` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
 CREATE TABLE `Post` (
     `id` VARCHAR(191) NOT NULL,
     `title` VARCHAR(191) NOT NULL,
