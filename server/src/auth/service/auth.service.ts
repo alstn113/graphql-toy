b54b2439d03d50fd2a28884523ba0aba82e9d6bc
neko113 <alstn113@gmail.com>
2022-06-28 17:54:18 +0900

    feat : auth service settings

--- server/src/auth/service/auth.service.ts
+++ server/src/auth/service/auth.service.ts
@@ -1,7 +1,45 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
 import { PrismaService } from 'prisma/prisma.service';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly prisma: PrismaService) {}
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly jwtServcie: JwtService,
+    private readonly configServcie: ConfigService,
+  ) {}
+
+  async logout() {
+    return;
+  }
+
+  async refresh() {
+    return;
+  }
+
+  hashData() {
+    return;
+  }
+
+  compareData() {
+    return;
+  }
+
+  getTokens() {
+    return;
+  }
+
+  updateRtHash() {
+    return;
+  }
+
+  setTokenCookie() {
+    return;
+  }
+
+  clearTokenCookie() {
+    return;
+  }
 }
